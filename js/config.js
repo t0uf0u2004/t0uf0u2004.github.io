c6827d3327a79461ada1f9fcdac93a1a771d8f2c
t0uf0u2004 <81165286+t0uf0u2004@users.noreply.github.com>
2023-11-24 17:18:43 +0100

    config.js

--- js/config.js
+++ js/config.js
@@ -138,7 +138,7 @@ var l_musicRandom = true;
 
 // Music volume
 // Choose a value between 0 (quietest) and 100 (loudest)
-var l_musicVolume = 20;
+var l_musicVolume = 5;
 
 // Enable custom messages?
 // 'true' or 'false'
